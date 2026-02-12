696c114d8a5da290eaa1a6568a20415780979118
Malik Beaudoin <malik.beaudoin@hotmail.com>
2026-02-12 15:32:42 -0500

    modified auth.js to change redirection page based on type of user in SignIn.html. Please change Main.html to Student.html when possible

--- auth.js
+++ auth.js
@@ -10,7 +10,13 @@ function signUp(event) {
     const firstName = document.getElementById('FirstName').value;
     const email = document.getElementById('UserEmail').value;
     const password = document.getElementById('UserPassword').value;
-    const typeOfUser = document.querySelector('input[name="TypeOfUser"]').value;
+    const checkedRole = document.querySelector('input[name="TypeOfUser"]:checked');
+
+    if(!checkedRole){
+        alert('Please Choose either Student or Admin.');
+        return;
+    }
+    const typeOfUser = checkedRole.value;
 
     // store in local storage (database soon)
     localStorage.setItem('firstName', firstName);
@@ -39,10 +45,17 @@ function signIn(event) {
 
     const storedEmail = localStorage.getItem('email');
     const storedPassword = localStorage.getItem('password');
+    const storedType = localStorage.getItem('typeOfUser'); // Student or Admin
 
     if (email === storedEmail && password === storedPassword) {
         alert('Sign-in successful!');
-        window.location.href = 'Main.html';
+        if(storedType === "Admin"){
+            window.location.href = 'Admin.html';
+        } else if (storedType === "Student"){
+            window.location.href = 'Main.html';
+        } else{
+            window.location.href = 'Login.html';
+        }
     }
     else {
         alert('Invalid email or password. Please try again.');
