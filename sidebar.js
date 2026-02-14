159ebd8ab9c766055f856b5dcf8550f8a69db18d
Strykerxman <strykerxman@gmail.com>
2026-02-14 15:52:32 -0500

    added admin.html with tabs for creating, removing, enable/disable courses.

---
+++ sidebar.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("profile_sidebar").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("profile_sidebar").style.width = "0";
+  document.getElementById("profile_sidebar_btn").style.marginLeft = "0";
+} 
